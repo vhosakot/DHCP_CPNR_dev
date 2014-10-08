dafe2f9131d73f5439c6f7332427dfb88a789fb3
Vikram G Hosakote <vhosakot@cisco.com>
2014-10-08 13:23:41 -0400

    Added more comments

--- CPNR_restAPI_test_client.py
+++ CPNR_restAPI_test_client.py
@@ -7,17 +7,20 @@
 import pprint
 from CPNR_restAPI import CPNR_restApi
 
+# CPNR server info
 CPNRip = "192.168.122.247"
 CPNRport = 8080
 CPNRusername = "cpnradmin"
 CPNRpassword = "password"
 
+# Create CPNR_restApi object
 c = CPNR_restApi(CPNRip, CPNRport, CPNRusername, CPNRpassword)
 
 print c
 print CPNR_restApi(CPNRip, CPNRport, CPNRusername, CPNRpassword)
 print dir(c)
 
+# Check docstrings of all methods
 print c.__doc__
 print c.__init__.__doc__
 print c.get_DHCPServer.__doc__
@@ -34,6 +37,7 @@ print c.get_client_entry.__doc__
 print c.create_policy.__doc__
 print c.create_scope.__doc__
 
+# Print all attributes
 print "c.CPNR_server_ip       = {0}".format(c.CPNR_server_ip)
 print "c.CPNR_server_port     = {0}".format(c.CPNR_server_port)
 print "c.CPNR_server_username = {0}".format(c.CPNR_server_username)
@@ -41,38 +45,49 @@ print "c.auth                 = {0}".format(c.auth)
 print "c.headers              = {0}".format(c.headers)
 print "c.url                  = {0}".format(c.url)
 
+# Get DHCPServer
 DHCPServer = c.get_DHCPServer()
 pprint.pprint(DHCPServer)
 
+# Get all policies
 Polices = c.get_policies()
 pprint.pprint(Polices)
 
+# Get a specific policy
 policy1 = c.get_policy("system_default_policy")
 pprint.pprint(policy1)
 policy2 = c.get_policy("default")
 pprint.pprint(policy2)
 
+# Get all ClientClasses
 ClientClasses = c.get_client_classes()
 pprint.pprint(ClientClasses)
 
+# Get a specific ClientClass
 ClientClass = c.get_client_class()
 pprint.pprint(ClientClass)
 
+# Get all VPNs
 VPNs = c.get_vpns()
 pprint.pprint(VPNs)
 
+# Get a specific VPN
 VPN = c.get_vpn()
 pprint.pprint(VPN)
 
+# Get all scopes
 Scopes = c.get_scopes()
 pprint.pprint(Scopes)
 
+# Get a specific scope
 Scope = c.get_scope()
 pprint.pprint(Scope)
 
+# Get all ClientEntries
 ClientEntries = c.get_client_entries()
 pprint.pprint(ClientEntries)
 
+# Get a specific ClientEntry
 ClientEntry = c.get_client_entry()
 pprint.pprint(ClientEntry)
 
@@ -81,6 +96,7 @@ data = {"name":"policy1", "optionList":{"OptionItem":[{"number":"500","value":"0
 print c.create_policy(data)
 data = {"name":"policy2", "optionList":{"OptionItem":[{"number":"600","value":"00:09:3a:83"}]}}
 print c.create_policy(data)
+# Get all policies
 pprint.pprint(c.get_policies())
 
 # Create scope object with name, subnet, restrictToReservations, vpnId
@@ -88,5 +104,6 @@ data = {"name":"scope1", "subnet":"2.2.2.2/24", "restrictToReservations":"True",
 print c.create_scope(data)
 data = {"name":"scope2", "subnet":"3.3.3.3/24", "restrictToReservations":"True", "vpnId":"40"}
 print c.create_scope(data)
+# Get all scopes
 pprint.pprint(c.get_scopes())
 
