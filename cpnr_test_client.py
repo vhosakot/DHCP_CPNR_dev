ec25713fb6deccf3f877d246cca707e2d40c7e73
Vikram G Hosakote <vhosakot@cisco.com>
2014-10-15 10:15:53 -0400

    Added _do_request

--- cpnr_test_client.py
+++ cpnr_test_client.py
@@ -1,12 +1,26 @@
 #! /usr/bin/python
 
+# Copyright 2014 Cisco Systems, Inc.  All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 ########
 # Test client to unit-test CPNR_restAPI.py
 ########
 
 import sys
 import pprint
-from CPNR_restAPI import CPNR_restApi
+from cisco_cpnr_rest_client import CpnrRestClient
 
 # CPNR server info
 CPNRip = "192.168.122.247"
@@ -14,14 +28,14 @@ CPNRport = 8080
 CPNRusername = "cpnradmin"
 CPNRpassword = "password"
 
-# Create CPNR_restApi object
-c = CPNR_restApi(CPNRip, CPNRport, CPNRusername, CPNRpassword)
+# Create CpnrRestClient object
+c = CpnrRestClient(CPNRip, CPNRport, CPNRusername, CPNRpassword)
 
-# Print CPNR_restApi object
-test_name = "Print CPNR_restApi object"
+# Print CpnrRestClient object
+test_name = "Print CpnrRestClient object"
 print "\n======== {0} ========\n".format(test_name)
 print c
-print CPNR_restApi(CPNRip, CPNRport, CPNRusername, CPNRpassword)
+print CpnrRestClient(CPNRip, CPNRport, CPNRusername, CPNRpassword)
 print dir(c)
 
 # Check docstrings of all methods
